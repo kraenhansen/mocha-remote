e2b8b973be74ff40021e968779854b935d9ab9db
Kræn Hansen <kraen.hansen@realm.io>
2018-07-21 22:57:17 +0200

    Using prepublish instead of postinstall

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,5 +1,7 @@
 const { resolve } = require("path");
 
+// This script should be run after a user accidentally installs the root package
+
 const parentPath = resolve(__dirname, '../..');
 
 if (/node_modules$/.test(parentPath)) {
