dbd734edc38929a7710399b0451a31c262b123f6
Kræn Hansen <kraen.hansen@realm.io>
2018-07-23 16:10:10 +0200

    Including the cli when setting versions

--- scripts/update-versions.js
+++ scripts/update-versions.js
@@ -2,6 +2,11 @@ const cp = require("child_process");
 const { resolve } = require("path");
 
 const { version } = require("../package.json");
+// Update the cli version
+cp.spawnSync("npm", [ "version", version, "--allow-same-version" ], {
+  cwd: resolve(__dirname, "../packages/cli"),
+  stdio: ['inherit', 'inherit', 'inherit']
+});
 // Update the client version
 cp.spawnSync("npm", [ "version", version, "--allow-same-version" ], {
   cwd: resolve(__dirname, "../packages/client"),
