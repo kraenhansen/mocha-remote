70bcbfb9614e7b6cb2522419637ffee59e28833b
Kræn Hansen <kraen.hansen@mongodb.com>
2023-11-01 10:29:44 +0100

    Tried adding in some logging

--- packages/cli/src/test/throwing-client.ts
+++ packages/cli/src/test/throwing-client.ts
@@ -4,4 +4,7 @@ new Client({
   tests: () => {
     throw new Error("b00m!");
   }
+}).on("error", err => {
+  // eslint-disable-next-line no-console
+  console.log(`An error occurred: ${err.message}`);
 });
