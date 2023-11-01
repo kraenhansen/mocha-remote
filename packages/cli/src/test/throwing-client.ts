a5bab1980a80aa1dd5cf01f193f1a51f4b02965f
Kræn Hansen <kraen.hansen@mongodb.com>
2023-11-01 10:36:23 +0100

    Try disconnecting

--- packages/cli/src/test/throwing-client.ts
+++ packages/cli/src/test/throwing-client.ts
@@ -1,11 +1,11 @@
 import { Client } from "mocha-remote-client";
 
-new Client({
+const client = new Client({
   tests: () => {
     throw new Error("b00m!");
   }
 }).on("error", err => {
   // eslint-disable-next-line no-console
   console.log(`An error occurred: ${err.message}`);
-  process.exit();
+  client.disconnect();
 });
